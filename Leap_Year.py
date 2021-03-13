7f2c726ec3436d65a6ff29f58f26ed6d2489817b
Rodney L LaLonde <76617893+eon9eon9@users.noreply.github.com>
2021-03-12 21:44:59 -0600

    Add files via upload

---
+++ Leap_Year.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+current_year = int(input("Enter the current year: "))
+future_year = int(input("Enter a year in the future: "))
+
+if future_year <= current_year:
+    print("The year provided is not in the future.")
+else:
+    years_between = future_year - current_year
+    if future_year % 4 == 0:
+        if future_year % 100 == 0:
+            if future_year % 400 == 0:
+                print(future_year, "is a leap year, and it is", years_between, "years away")
+            else:
+                print(future_year, "is a not a leap year")
+        else:
+            print(future_year, "is a leap year, and it is", years_between, "years away")
+    else:
+        print(future_year, "is a leap year, and it is", years_between, "years away")
