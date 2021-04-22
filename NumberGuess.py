f1fe214fee78fb790b5ed332c829b7ef05f5d0e9
Rodney L LaLonde <76617893+eon9eon9@users.noreply.github.com>
2021-04-22 13:22:49 -0500

    Add files via upload
    Number guess game were you try to guess number between 1-100. It's working mostly.

--- NumberGuess.py
+++ NumberGuess.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 #Number Guess
-import sys
 import math
-import string
-SecretNumber = 70
-Numbers = range(100)
-User_Input = input("Enter a number between 1 through 100 :  "  )
 
-while input != 70:
-   print("Wrong guess try again")
-   if int(User_Input) > 70:
-      print("Lower")
-   elif int(User_Input) < 70:
-      print("Higher")
+secret_number = "70"
+guess = input(" ")
+
+while guess != secret_number:
+   guess=input("Guess secret number between 1 to 100: ")
+   if  guess > "70":
+     print("Lower")
+   elif guess < "70":
+     print("Higher")
    else:
-     print( "Congradulations" )
+      print( "Congradulations" )
 
