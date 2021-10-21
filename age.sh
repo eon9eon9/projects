82af81b1dbe12cd761355c0a6f0afe6facd082a4
eon9 <76617893+eon9eon9@users.noreply.github.com>
2021-10-21 08:17:24 -0500

    Add files via upload

---
+++ age.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+age = 40
+
+if [ "$age -gt 40" ] 
+    echo "Lower"
+
+else [ "$age -lt 40" ]
+        echo "Higher"
+elif [ "$age == 40" ]
+        echo "Congratulations"
+fi        
